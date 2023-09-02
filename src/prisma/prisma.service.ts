98d0cd3ed5e566fac76dbef881cdf7a39a95de65
Yan Soares <yts.1995@gmail.com>
2023-09-02 13:15:05 -0300

    chore: add prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super()
+  }
+
+  onModuleInit() {
+    return this.$connect()
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect()
+  }
+}
