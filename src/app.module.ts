d9d60023b0f192aa9e997ace6be494b65c82e635
Yan Soares <yts.1995@gmail.com>
2023-09-07 17:43:58 -0300

    chore: add jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from 'src/auth/auth.module'
 import { CreateAccountController } from 'src/controllers/create-account.controller'
 import { envSchema } from 'src/env'
 import { PrismaService } from 'src/prisma/prisma.service'
@@ -10,6 +11,7 @@ import { PrismaService } from 'src/prisma/prisma.service'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
