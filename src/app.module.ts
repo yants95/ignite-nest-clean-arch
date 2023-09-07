ad09093c28e6d48788be1c25fc0e0edd2c99c548
Yan Soares <yts.1995@gmail.com>
2023-09-07 15:05:33 -0300

    chore: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { CreateAccountController } from 'src/controllers/create-account.controller'
+import { envSchema } from 'src/env'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
