3a69328e5bff32ad98eaa9a6568b18a175359ebf
Yan Soares <yts.1995@gmail.com>
2023-09-17 15:28:55 -0300

    test: add create-account and authenticate controller e2e tests

---
+++ tests/e2e/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,28 @@
+import { AppModule } from '@/app.module'
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('CreateAccountController (e2e)', () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    await app.init()
+  })
+
+  test('POST /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'John Doe',
+      email: 'johndoe@email.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
