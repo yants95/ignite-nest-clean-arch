be34047e8c9ddb1805fbcfdc078ddcff442d586d
Yan Soares <yts.1995@gmail.com>
2023-10-15 18:48:42 -0300

    feat: add choose-question-best-answer controller and e2e test

---
+++ src/infra/http/controllers/choose-question-best-answer.controller.ts
@@ -0,0 +1,31 @@
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  Param,
+  Patch,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/application/use-cases/choose-question-best-answer'
+
+@Controller('/answers/:answerId/choose-as-best')
+export class ChooseQuestionBestAnswerController {
+  constructor(private readonly usecase: ChooseQuestionBestAnswerUseCase) {}
+
+  @Patch()
+  @HttpCode(204)
+  async handle(
+    @CurrentUser() user: UserPayload,
+    @Param('answerId') answerId: string,
+  ) {
+    const userId = user.sub
+
+    const result = await this.usecase.execute({
+      authorId: userId,
+      answerId,
+    })
+
+    if (result.isLeft()) throw new BadRequestException()
+  }
+}
---
+++ tests/e2e/infra/http/controllers/e2e/choose-question-best-answer.controller.e2e-spec.ts
@@ -0,0 +1,56 @@
+import { AnswerFactory } from '#/unit/factories/make-answer'
+import { QuestionFactory } from '#/unit/factories/make-question'
+import { StudentFactory } from '#/unit/factories/make-student'
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+
+describe('ChooseQuestionBestAnswerController (e2e)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('PATCH /answers/:answerId/chooose-as-best', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const question = await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+    })
+
+    const answer = await answerFactory.makePrismaAnswer({
+      questionId: question.id,
+      authorId: user.id,
+    })
+
+    const answerId = answer.id.toString()
+
+    const response = await request(app.getHttpServer())
+      .patch(`/answers/${answerId}/choose-as-best`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(204)
+  })
+})
